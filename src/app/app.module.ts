6d0f3844f831007ae9ab82a370f9827d4573732d
you@example.com <you@example.com>
2022-07-09 09:11:01 +0000

    modif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBMZW5wsRjlmekcZ6nR40FZd50j90o23o",
+  authDomain: "pressingapp-4c94a.firebaseapp.com",
+  databaseURL: "https://pressingapp-4c94a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pressingapp-4c94a",
+  storageBucket: "pressingapp-4c94a.appspot.com",
+  messagingSenderId: "514280661169",
+  appId: "1:514280661169:web:8859db27d10dbf34024290",
+  measurementId: "G-XLSR01KBBR"
+};
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [ BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(firebaseConfig),AngularFirestoreModule ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  items: Observable<any[]>
 
-  constructor() {}
+  constructor(
+    public firestore : AngularFirestore
+  ) {
+    this.items = this.firestore.collection('users').valueChanges();
+  }
 
 }
